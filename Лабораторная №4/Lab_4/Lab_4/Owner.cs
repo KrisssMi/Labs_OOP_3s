54b08df429dfcca0d8bd30e889588cee74d0306d
Kristina <87947917+KrisssMi@users.noreply.github.com>
2021-10-07 00:18:02 +0300

    DONE

---
+++ Лабораторная №4/Lab_4/Lab_4/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    public class Owner
+    {
+        public int id = 9;
+        public string name = "Kristina Minevich";
+        public string organization = "FIT, BSTU";
+    }
+}
