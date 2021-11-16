3daf19002b5967d906a672c66f6d7c5df33291a4
Kristina <87947917+KrisssMi@users.noreply.github.com>
2021-11-17 01:11:54 +0300

    DONE

---
+++ Лабораторная №9/Lab_9_new/Lab_9_new/Items.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_new
+{
+	public class Items
+	{
+        public Items()
+        {
+        }
+
+        public Items(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; } = "Default";
+        public string Life { get; set; } = "5.0";
+        public bool IsLiving { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} has lifes {Life}. It's living now - {IsLiving}");
+        }
+
+        public void ChangeLife(string newLife)
+        {
+            Life = newLife;
+        }
+
+        public void living_true()
+        {
+            IsLiving = true;
+        }
+
+        public void living_false()
+        {
+            IsLiving = false;
+        }
+    }
+}
---
+++ Лабораторная №9/Lab_9_new/Lab_9_new/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab_9_new
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            var first_object = new Items("Мирный житель");
+            var second_object = new Items("Мафия");
+           
+            Game.OnHeal += first_object.ChangeLife;
+            first_object.ShowInfo();
+
+            Game.OnAttack += second_object.ChangeLife;
+            second_object.ShowInfo();
+
+            Console.WriteLine();
+
+            Game.Attack(first_object, "4.0");
+            first_object.ShowInfo();
+
+            Game.StartWorkWithItem(first_object);
+            first_object.ShowInfo();
+
+            Game.EndWorkWithItem(first_object);
+            first_object.ShowInfo();
+
+            Game.Heal(second_object, "6.0");
+            second_object.ShowInfo();
+        }
+	}
+}
